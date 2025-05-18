f5692d46e6cedc3717be1c6fbea7fc1493f70d6a
Vitor Marvulle <marvullevitor@gmail.com>
2025-05-18 19:11:30 -0300

    add: fluxo de paginas e estilizacao simples

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.app, name="home"),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('quantidade/', views.quantidade, name='quantidade'),
+    path('formulario/', views.formulario, name='formulario'),
+    path('pagamento/', views.pagamento, name='pagamento'),
+    path('bilhete/', views.bilhete, name='bilhete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
-def app(request):
-    return render(request, 'home.html')
+def home(request):
+    return render(request, 'home.html', {
+        'next_url': '/quantidade/',
+    })
+
+def quantidade(request):
+    return render(request, 'quantidade.html', {
+        'previous_url': '/',
+        'next_url': '/formulario/',
+    })
+
+def formulario(request):
+    return render(request, 'formulario.html', {
+        'previous_url': '/quantidade/',
+        'next_url': '/pagamento/',
+    })
+
+def pagamento(request):
+    return render(request, 'pagamento.html', {
+        'previous_url': '/formulario/',
+        'next_url': '/bilhete/',
+    })
+
+def bilhete(request):
+    return render(request, 'bilhete.html', {
+        'previous_url': '/pagamento/',
+        'next_url': '/',
+    })
