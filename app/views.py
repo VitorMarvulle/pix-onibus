57c4a230ca9674711c1262349a4e80c26434b6cb
Gabriel Rigoni <rigonigabriel12@gmail.com>
2025-05-25 21:12:40 -0300

    feat1: qrCode Pix adicionado

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import mercadopago, base64, qrcode
+from io import BytesIO
 
 def home(request):
     return render(request, 'home.html', {
         'next_url': '/quantidade/',
+        'empresas': '/empresa/login',
     })
 
 def quantidade(request):
@@ -18,9 +21,43 @@ def formulario(request):
     })
 
 def pagamento(request):
+    sdk = mercadopago.SDK("TEST-6337997335997184-040119-fce8a6dd49cf5dcc02d0ef2414c85283-1489971308")
+
+    payment_data = {
+        "transaction_amount": 10.00,
+        "description": "Pagamento de passagem",
+        "payment_method_id": "pix",
+        "payer": {
+            "email": "comprador@email.com",
+            "first_name": "Nome",
+            "last_name": "Sobrenome",
+            "identification": {
+                "type": "CPF",
+                "number": "12345678909"
+            },
+            "address": {
+                "zip_code": "06233200",
+                "street_name": "Av. das Nações Unidas",
+                "street_number": "3003",
+                "neighborhood": "Bonfim",
+                "city": "Osasco",
+                "federal_unit": "SP"
+            }
+        }
+    }
+    payment_response = sdk.payment().create(payment_data)
+    payment = payment_response["response"]
+    pix_payload = payment["point_of_interaction"]["transaction_data"]["qr_code"]
+
+    qr = qrcode.make(pix_payload)
+    buffer = BytesIO()
+    qr.save(buffer, format="PNG")
+    img_base64 = base64.b64encode(buffer.getvalue()).decode()
+
     return render(request, 'pagamento.html', {
         'previous_url': '/formulario/',
         'next_url': '/bilhete/',
+        'qrcode_base64': img_base64,
     })
 
 def bilhete(request):
