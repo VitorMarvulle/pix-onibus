0287462e5d2095dca2c0f675cd7e66dfe1e1a996
Gabriel Rigoni <rigonigabriel12@gmail.com>
2025-06-07 11:15:33 -0300

    feat: bilhete gerado parte 1

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('quantidade/', views.quantidade, name='quantidade'),
     path('formulario/', csrf_exempt(views.formulario), name='formulario'),
     path('pagamento/', csrf_exempt(views.pagamento), name='pagamento'),
+    path('check-payment-status/', views.check_payment_status, name='check_payment_status'),
     path('bilhete/', views.bilhete, name='bilhete'),
 ]
