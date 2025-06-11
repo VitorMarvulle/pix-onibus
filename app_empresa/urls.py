cc102949d429588d2c942bca7826994952f214ba
Vitor Marvulle <112207002+VitorMarvulle@users.noreply.github.com>
2025-06-10 22:43:52 -0300

    Qrcode fix (#7)
    * alteracoes teste
    
    * alteracoes ngrok para rodar local no celular
    
    * feat: qrcode funcionando e validacao passagens!!!

--- app_empresa/urls.py
+++ app_empresa/urls.py
@@ -8,11 +8,15 @@ urlpatterns = [
     path('editar_linha/<int:id>/', views.editar_linha, name='editar_linha'),
     path('excluir_linha/<int:id>/',views.excluir_linha, name='excluir_linha'),
     path('add_motorista/', views.add_motorista, name='add_motorista'),
+
+    # app_Empresa
     path('dashboard/', views.dashboard, name='dashboard'),
     path('home_motorista/', views.home_motorista, name='home_motorista'),
     path('lista_motorista/', views.lista_motorista, name='lista_motorista'),
     path('linha_motorista/', views.linha_motorista, name='linha_motorista'),
     path('login/', views.login, name='login'),
     path('confirmacao/', views.confirmacao, name='confirmacao'),
+    
+    path('api/validar-bilhete/', views.validar_bilhete, name='validar_bilhete'),
 
 ]
--- pix_onibus/settings.py
+++ pix_onibus/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','172.20.10.4', '03da-200-173-56-111.ngrok-free.app']
 
 
 # Application definition
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
